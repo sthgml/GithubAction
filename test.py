553bed2e288300dc5c5b5daceb46ceb9880fc84d
Sohee_Park <41767015+sthgml@users.noreply.github.com>
2024-03-23 14:33:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,6 +8,6 @@ html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('.title_text')
+bookservices = soup.select('.fig-oqedbt')
 for no, book in enumerate(bookservices, 1):
     print(no, book.text.strip())
