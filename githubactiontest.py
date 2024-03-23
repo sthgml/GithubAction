090b91a14f30273bb1fc1c106d24e521d551ab76
Sohee_Park <41767015+sthgml@users.noreply.github.com>
2024-03-23 16:35:04 +0900

    Update githubactiontest.py

--- githubactiontest.py
+++ githubactiontest.py
@@ -1,13 +1,35 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/new-releases/2200'
-response = requests.get(url)
+response = requests.get("http://paullab.synology.me/stock.html")
+
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('.fig-oqedbt')
-for no, book in enumerate(bookservices, 1):
-    print(no, book.text.strip())
+oneStep = soup.select('.main')[2]
+twoStep = oneStep.select('tbody > tr')[1:]
+
+날짜 = []
+종가 = []
+전일비 = []
+거래량 = []
+
+for i in twoStep:
+    날짜.append(i.select('td')[0].text)
+    종가.append(int(i.select('td')[1].text.replace(',', '')))
+    전일비.append(int(i.select('td')[2].text.replace(',', '')))
+    거래량.append(int(i.select('td')[6].text.replace(',', '')))
+
+l = []
+
+for i in range(len(날짜)):
+    l.append({
+        '날짜':날짜[i],
+        '종가':종가[i],
+        '전일비':전일비[i],
+        '거래량':거래량[i],
+        })
+
+print(l)
