52ad7afbcd040b8c76e49dc1527fb396f83dd1bc
Sohee_Park <41767015+sthgml@users.noreply.github.com>
2024-03-23 17:10:04 +0900

    Update githubactiontest.py

--- githubactiontest.py
+++ githubactiontest.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
+import json
 
-response = requests.get("http://paullab.synology.me/stock.html")
+response = requests.get("https://paullab.co.kr/stock.html")
 
 response.encoding = 'utf-8'
 html = response.text
@@ -32,4 +34,19 @@ for i in range(len(날짜)):
         '거래량':거래량[i],
         })
 
-print(l)
+#파일을 한 번 쓴다.
+with open('data.js', "w", encoding="UTF-8-sig") as f_write:
+    json.dump(l, f_write, ensure_ascii=False, indent=4)
+
+#파일을 다시 읽는다.
+data = ""
+with open('data.js', "r", encoding="UTF-8-sig") as f:
+    line = f.readline()
+    while line:
+        data += line
+        line = f.readline()
+
+#파일에 변수명을 추가하여 다시 쓴다.
+final_data = f"var data = {data};"
+with open('data.js', "w", encoding="UTF-8-sig") as f_write:
+    f_write.write(final_data)
